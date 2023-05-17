dc7b14ff25e79dbc2821ccb5904af129b4eab776
krashanoff <leo@krashanoff.com>
2023-05-17 09:41:16 -0700

    Never mind about orgmode

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -345,27 +345,27 @@ return require('lazy').setup({
           })
       end
   },
-  {
-      'nvim-orgmode/orgmode',
-      dependencies = { 'nvim-treesitter/nvim-treesitter' },
-      config = function()
-          require('orgmode').setup_ts_grammar()
-          require('nvim-treesitter.configs').setup {
-              -- If TS highlights are not enabled at all, or disabled via `disable` prop,
-              -- highlighting will fallback to default Vim syntax highlighting
-              highlight = {
-                enable = true,
-                -- Required for spellcheck, some LaTex highlights and
-                -- code block highlights that do not have ts grammar
-                additional_vim_regex_highlighting = {'org'},
-              },
-              ensure_installed = {'org'}, -- Or run :TSUpdate org
-          }
-          require('orgmode').setup{
-              org_agenda_files = { '~/org/**/*' },
-          }
-      end
-  }
+  -- {
+  --     'nvim-orgmode/orgmode',
+  --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  --     config = function()
+  --         require('orgmode').setup_ts_grammar()
+  --         require('nvim-treesitter.configs').setup {
+  --             -- If TS highlights are not enabled at all, or disabled via `disable` prop,
+  --             -- highlighting will fallback to default Vim syntax highlighting
+  --             highlight = {
+  --               enable = true,
+  --               -- Required for spellcheck, some LaTex highlights and
+  --               -- code block highlights that do not have ts grammar
+  --               additional_vim_regex_highlighting = {'org'},
+  --             },
+  --             ensure_installed = {'org'}, -- Or run :TSUpdate org
+  --         }
+  --         require('orgmode').setup{
+  --             org_agenda_files = { '~/org/**/*' },
+  --         }
+  --     end
+  -- }
 })
 
 -- color scheme :)
