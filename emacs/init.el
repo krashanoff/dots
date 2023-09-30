6d233219ab1874554b6917348544a8dd81998818
krashanoff <leo@krashanoff.com>
2023-09-30 10:44:03 -0700

    Blah

--- emacs/init.el
+++ emacs/init.el
@@ -14,7 +14,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
- '(package-selected-packages '(use-package magit general evil)))
+ '(package-selected-packages '(transpose-frame use-package magit general evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
