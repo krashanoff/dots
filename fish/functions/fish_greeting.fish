06afa4219bb2541209aba2ab84653a591f506ce7
krashanoff <leo@krashanoff.com>
2024-01-22 20:26:46 -0800

    Add custom greeting

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,12 @@
+function fish_greeting
+    if set -q fish_private_mode
+        set -l line (_ "fish is running in private mode, history will not be persisted.")
+        if set -q fish_greeting[1]
+            set -g fish_greeting $fish_greeting\n$line
+        else
+            set -g fish_greeting $line
+        end
+    end
+
+    echo (set_color yellow; date +%T; set_color normal)
+end
