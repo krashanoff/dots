3419e621d0c63123ea6900781bb10786541975c5
krashanoff <leo@krashanoff.com>
2024-07-13 18:05:38 -0700

    Some emacs changes

---
+++ emacs/init.el
@@ -0,0 +1,25 @@
+;; Just enough to bootstrap my config so that it understands
+;; how to untangle Orgmode files.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'org)
+  (package-install 'org))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
+ '(package-selected-packages '(transpose-frame use-package magit general evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(org-babel-load-file "~/.config/emacs/config.org")
